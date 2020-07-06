f4b7d5a9148fdaf056e0d0ec6631603bb5d4d438
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-07-06 17:36:36 -0400

    Created Footer and Home component. Added basic structure.

---
+++ client/src/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className= "home">
+            <h1 className="title">Cork It</h1>
+
+            <div>
+                <h3>- Plan</h3>
+                <h3>- Create</h3>
+                <h3>- Enjoy</h3>
+
+                <button> Login</button>
+                <button> Sign Up</button>
+            </div>
+            
+        </div>
+    )
+}
---
+++ client/src/shared/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            &copy; 2020 Jenna Rysanek
+        </footer>
+    )
+}
