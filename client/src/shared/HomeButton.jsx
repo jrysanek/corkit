0c324690b4ab50eb219c02b0710a4d7b9206ecc4
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-07-09 20:21:39 -0400

    Added onClick to HomeButton component

--- client/src/shared/HomeButton.jsx
+++ client/src/shared/HomeButton.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function HomeButton() {
+
+    const history = useHistory();
+
+    function handleClickHome() {
+        history.push('/')
+    }
     return (
-        <button className='home-button'>
+        <button className='home-button' onClick={handleClickHome}>
             Home
         </button>
     )
