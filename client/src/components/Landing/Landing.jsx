57290e18c7f5911e5c3236f3d194f61969c3abf4
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-07-14 19:41:03 -0400

    Created LandingCards component

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../shared/Header'
 import HomeButton from '../../shared/HomeButton'
+import LandingCards from './LandingCards'
 
 export default function Landing() {
     return (
@@ -12,12 +13,8 @@ export default function Landing() {
                 <button>Sign Out</button>
             </div> 
 
-            <div className="landing-content">
-                <p>Image will be here</p>
-                <h6>Room Name</h6>
-                <button>Edit</button>
-                <button>Delete</button>
-
+            <div>
+                <LandingCards />
             </div>
 
         </div>
---
+++ client/src/components/Landing/LandingCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LandingCards() {
+    return (
+        <div className="landing-cards">
+            <p>Image will be here</p>
+            <h6>Room Name</h6>
+            <button>Edit</button>
+            <button>Delete</button>
+
+         </div>
+    )
+}
