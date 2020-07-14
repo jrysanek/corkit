74f3ed49455c47686445a58e69359587e8a92452
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-07-14 15:43:07 -0400

    Created handle functions for login. Connected Login FE to BE- connection works.

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,22 +1,51 @@
-import React from 'react'
-import Header from '../../shared/Header'
-import HomeButton from '../../shared/HomeButton'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Header from '../../shared/Header';
+import HomeButton from '../../shared/HomeButton';
+import { loginUser } from '../../services/auth';
 
 export default function Login() {
-    
+    let history = useHistory()
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleInput = (e) => {
+        let { name, value } = e.target
+        setForm(prevState => ({
+            ...prevState,
+            [name]: value
+        })
+        )
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try { 
+            loginUser(form) 
+            history.push('/landing')
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
 
     return (
         <div className="login-container">
             <Header />
 
             <div className="login-form">
-                <input placeholder='Email'></input>
-                <input placeholder='Password'></input>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <input name='email' value={form.email} onChange={handleInput} placeholder='Email'></input>
+                    <input name='password' value={form.password} onChange={handleInput} placeholder='Password'></input>
 
-                <button>Login</button>
+                    <button>Login</button>
+                </form>
                 <HomeButton />
+
             </div>
-            
+
         </div>
     )
 }
