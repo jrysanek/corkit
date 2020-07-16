ad630d1502b475f5bbac5bfd104d0569d5bb37e5
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-07-16 17:15:04 -0400

    Added function to handle tracking characters enterd into input boxes on SignUp component.

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.css';
 import Header from '../../shared/Header';
 import HomeButton from '../../shared/HomeButton';
 
 export default function SignUp() {
+    const [ newForm, setNewForm ] = useState({
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleNewInput = (e) => {
+        let { name, value } = e.target
+        setNewForm(prevState => ({
+            ...prevState,
+            [name]: value
+        })
+        )
+    }
 
     return (
         <div className="sign-up-container">
@@ -12,9 +26,27 @@ export default function SignUp() {
 
             <div className="sign-up-form">
                 <form>
-                    <input placeholder='Email'></input>
-                    <input placeholder='Password'></input>
-                    <input placeholder='Confirm Password'></input>
+                    <input 
+                        name='email'
+                        onChange={handleNewInput} 
+                        value={newForm.email} 
+                        placeholder='Email'
+                    />
+                        
+                    <input 
+                        name='password'
+                        onChange={handleNewInput} 
+                        value={newForm.password} 
+                        placeholder='Password'
+                    />
+
+
+                    <input 
+                        name='confirmPassword'
+                        onChange={handleNewInput} 
+                        value={newForm.confirmPassword} 
+                        placeholder='Confirm Password'
+                    />
 
                     <button>Sign Up</button>
                 </form>
