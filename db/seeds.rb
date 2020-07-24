f5f7829552dfefef3763c356b976f89a7d7830f0
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-07-24 09:24:01 -0400

    commented out destroy all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Room.destory_all
-Board.destory_all
-User.destroy_all
+# Room.destory_all
+# Board.destory_all
+# User.destroy_all
 
 @user1 = User.create!({ email: 'jess@jess22.com', password: '123456' })
 
