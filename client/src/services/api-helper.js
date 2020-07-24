54d8b226b8ffb149d4f05c78c27ae38936b5f69a
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-07-24 09:53:18 -0400

    changed base url

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://cork-it-api.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
